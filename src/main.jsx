b29bb78a06398cbf84b12d677776ee88bfadcc08
Fits <44420231+yessirrr@users.noreply.github.com>
2025-09-18 20:43:36 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./App.css";
+import "./index.css"; // or "./App.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
